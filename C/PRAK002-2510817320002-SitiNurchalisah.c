f3d33d5be7cbc9c3c8f0e6685d4426c37b6629f5
caca9090 <alwayscoffe2@gmail.com>
2025-09-27 08:25:43 +0800

    Add files via upload

---
+++ C/PRAK002-2510817320002-SitiNurchalisah.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+printf("Selamat Pagi, Siti Nurchalisah\n");
+    printf("Selamat Siang, Siti Nurchalisah\n");
+    printf("Selamat Malam, Siti Nurchalisah\n");
+    return 0;
---
+++ C/PRAK004-2510817320002-SitiNurchalisah.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#       Siti Nurchalisah     #\n");
+    printf("#       2510817320002        #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
