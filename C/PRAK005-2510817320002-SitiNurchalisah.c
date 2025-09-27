4e951dd9ec01c9aba589b7a1b048bebf5e39855e
caca9090 <alwayscoffe2@gmail.com>
2025-09-27 15:35:29 +0800

    Update PRAK005-2510817320002-SitiNurchalisah.c

--- C/PRAK005-2510817320002-SitiNurchalisah.c
+++ C/PRAK005-2510817320002-SitiNurchalisah.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main() {
-    printf("##     #      ###### \n");
-    printf("###    #     #      #\n");
-    printf("## #   #     #      #\n");
-    printf("##  #  #     #      #\n");
-    printf("##   # #     #      #\n");
-    printf("##    ##     #      #\n");
-    printf("##     #      ###### \n");
+    printf(" #####    #     # \n");
+    printf("#         #     # \n");
+    printf("#         #     # \n");
+    printf(" #####    ####### \n");
+    printf("      #   #     # \n");
+    printf("      #   #     # \n");
+    printf(" #####    #     # \n");
     return 0;
 }
+
+
